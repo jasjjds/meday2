a00e947047f2a9503acfc383e3cb5cab805cc646
nguyendat <nguyendat08022004@gmail.com>
2025-08-09 15:20:57 +0700

    change generatiStaticParams1

--- src/app/dashboard/post/[title]/page.jsx
+++ src/app/dashboard/post/[title]/page.jsx
@@ -32,10 +32,11 @@ export default async function Page({ params }) {
  * NOTE: Remove all "generateStaticParams()" functions if not using static exports.
  */
 export async function generateStaticParams() {
-  const res = await axios.get(endpoints.post.list);
-  const data = CONFIG.isStaticExport ? res.data.posts : res.data.posts.slice(0, 1);
+  // const res = await axios.get(endpoints.post.list);
+  // const data = CONFIG.isStaticExport ? res.data.posts : res.data.posts.slice(0, 1);
 
-  return data.map((post) => ({
-    title: kebabCase(post.title),
-  }));
+  // return data.map((post) => ({
+  //   title: kebabCase(post.title),
+  // }));
+  return[]
 }
